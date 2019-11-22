4e95f8d4d5e1414de758c3fabd0ef36339299599
pleasewhy <1943788269@qq.com>
2019-11-22 13:20:02 +0800

    1.2

--- workspace-gateway/src/main/java/com/aliware/tianchi/UserLoadBalance.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/UserLoadBalance.java
@@ -21,10 +21,10 @@ public class UserLoadBalance implements LoadBalance {
 
     @Override
     public <T> Invoker<T> select(List<Invoker<T>> invokers, URL url, Invocation invocation) throws RpcException {
-        int tmp = ThreadLocalRandom.current().nextInt(15);
+        int tmp = ThreadLocalRandom.current().nextInt(6);
         if(tmp < 1)
         	return invokers.get(0);
-        else if(tmp < 6)
+        else if(tmp < 3)
         	return invokers.get(1);
         else
         	return invokers.get(2);
