3e50d6cce4e891094b6d250eecf62f89e9d2c089
pleasewhy <1943788269@qq.com>
2019-12-20 15:49:55 +0800

    1.10

--- workspace-provider/src/main/java/com/aliware/tianchi/TestServerFilter.java
+++ workspace-provider/src/main/java/com/aliware/tianchi/TestServerFilter.java
@@ -32,12 +32,9 @@ public class TestServerFilter implements Filter {
     @Override
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
         try{
-            totalCount++;
-            System.out.println(failCount);
             Result result = invoker.invoke(invocation);
             return result;
         }catch (Exception e){
-            TestServerFilter.failCount++;
             throw e;
         }
     }
